d73e0b9a11fcf959c4eb1e77ac54d529a152a02e
arthasjoshimar <jesuscallisayasalinas@gmail.com>
2023-03-14 23:23:23 -0400

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'zone.js/dist/zone';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -18,13 +18,29 @@ import { of, from } from 'rxjs';
 export class App implements OnInit {
 
   ngOnInit(): void {
-    of(2, 4, 6, 8).subscribe( item => console.log(item) );
+    //of(2, 4, 6, 8).subscribe( item => console.log(item) );
 
-    from([20, 15,10,5]).subscribe({
+    from([20, 15,10,5]).pipe(
+      tap( item => console.log(`emmited item ...${item}`) ),
+      map(item=> item * 2 ),
+      map(item => item - 10),
+      map(item => {
+        if(item === 0){
+          throw new Error('zero detected')
+        }
+        return item
+      }),
+      take(3)
+
+
+    ).subscribe({
       next: (item) => console.log(`resulting item .. ${item}`),
       error: (err) => console.log(`error occurred ${err}`),
       complete: ()=> console.log('complete')
     })
+
+
+
   }
   name = 'Angular';
 
